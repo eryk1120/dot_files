97b926e48f9754b02737a51ae16a95ffa2c42e5b
Eryk Salamonski <eryk.salamonski@protonmail.com>
2024-06-15 23:43:34 +0200

    refactor: 💡 remove comment block\

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -43,7 +43,6 @@ M.floating_terminal = {
 }
 
 M.trouble = {
-
 -- vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
 -- vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
 -- vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
